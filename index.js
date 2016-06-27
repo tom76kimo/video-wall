b7d3c9953d571338fbe1bf1f9dc76ad465e84831
Tom Wu <tom76kimo@yahoo-inc.com>
2016-06-27 17:20:26 +0800

    link to youtube api

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import GapiInit from './src/gapiInit';
+
+let globalParams = {};
+
+GapiInit(globalParams);
 
 class MainComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    globalParams.mainComponent = this;
+    this.state = {
+      apiLoaded: false
+    };
+  }
   render() {
     return (
-      <div>adsf</div>
+      <div>{this.state.apiLoaded ? 'API loaded' : 'nononono'}</div>
     );
   }
 }
---
+++ src/gapiInit.js
@@ -0,0 +1,32 @@
+const OAUTH2_CLIENT_ID = '208191556103-u0frivgcpr70vfpu287s32kjg4jtbgct.apps.googleusercontent.com';
+const OAUTH2_SCOPES = [
+  'https://www.googleapis.com/auth/youtube'
+];
+
+const gapiInit = (globalParams) => {
+  const checkAuth = () => {
+    gapi.auth.authorize({
+      client_id: OAUTH2_CLIENT_ID,
+      scope: OAUTH2_SCOPES,
+      immdiate: false
+    }, (authResult) => {
+      loadYoutubeAPI();
+    });
+  };
+
+  const loadYoutubeAPI = () => {
+    gapi.client.load('youtube', 'v3', () => {
+      globalParams.mainComponent.setState({
+        apiLoaded: true,
+      });
+    });
+  };
+
+  window.handleClientLoad = function () {
+    gapi.auth.init(() => {
+      setTimeout(checkAuth, 1);
+    });
+  };
+}
+
+export default gapiInit;
